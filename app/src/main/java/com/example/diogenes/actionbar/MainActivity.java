b194d4dc78c67db943ea049326e709fbf0feb6e1
HOME-DIO\Diógenes <diogenesr.freitas@gmail.com>
2018-05-02 16:40:44 -0300

    no message

---
+++ app/src/main/java/com/example/diogenes/actionbar/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.diogenes.actionbar;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Atualizando Título do ActionBar");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Infla o menu com os botões da action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_search) {
+            toast("Clicou no Pesquisar!");
+            return true;
+        } else if (id == R.id.action_refresh) {
+            toast("Clicou no Atualizar!");
+            return true;
+        } else if (id == R.id.action_settings) {
+            toast("Clicou no Configurações!");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void toast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
